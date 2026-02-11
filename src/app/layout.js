ab2cb0180fd6f0f16cba1df3390dc65ddde33307
jaicharan99 <agurijaicharan99@gmail.com>
2026-02-11 21:30:32 +0530

    added google verification meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "Chhaaya CBIT",
   description: "Film Fest Website",
+  verification: {
+    google: "google1cd30bda67faeb37.html",
+  },
+  
 };
 
 export default function RootLayout({ children }) {
